56997f1e41dd178529bfe5672282dddf5cfe524f
Daniel <dnl.canedo@gmail.com>
2018-05-04 12:33:12 -0400

    Add more properties to Track class

--- src/js/Track.js
+++ src/js/Track.js
@@ -8,11 +8,27 @@ export default class Track extends ListItem {
 
     settings = _.merge({
       artist: '[unknown artist]',
-      title: '[untitled]'
+      title: '[untitled]',
+      album: '[unknown album]',
+      artwork: null,
+      rating: 0
     }, settings);
 
     this._artist = settings.artist;
     this._title = settings.title;
+    this._album = settings.album;
+    this._artwork = settings.artwork;
+    this._rating = settings.rating;
+  }
+
+  getInfo () {
+    return {
+      artist: this._artist,
+      title: this._title,
+      album: this._album,
+      artwork: this._artwork,
+      rating: this._rating
+    };
   }
 
   _createHTML () {
