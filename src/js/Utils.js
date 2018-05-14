60590c8d3fab41150b0e858e93f37e6eb1d6eb60
Daniel <dnl.canedo@gmail.com>
2018-05-14 13:04:40 -0400

    Improve animation frame-rate

--- src/js/Utils.js
+++ src/js/Utils.js
@@ -2,18 +2,24 @@ export default {
   animate: (element, property, finalValue, callback) => {
     const finalValueNum = parseInt(finalValue, 10);
     const unit = finalValue.replace(finalValueNum, '');
-    const intervalRef = window.setInterval(() => {
-      let currentValue = parseInt(element.style[property], 10) || 0;
+    const callsPerSecond = 30;
+    const initialValue = parseInt(element.style[property], 10) || 0;
+    const diff = finalValueNum - initialValue;
+    const absDiff = Math.abs(diff);
+    const step = diff / callsPerSecond;
+    let traveled = 0;
 
-      if (currentValue === finalValueNum) {
+    const intervalRef = window.setInterval(() => {
+      traveled += step;
+
+      if (Math.abs(traveled) >= absDiff) {
         window.clearInterval(intervalRef);
+        element.style[property] = `${finalValueNum}${unit}`;
         return callback && callback();
-      } else if (currentValue < finalValueNum) {
-        element.style[property] = `${++currentValue}${unit}`;
       } else {
-        element.style[property] = `${--currentValue}${unit}`;
+        element.style[property] = `${initialValue + traveled}${unit}`;
       }
-    }, 1);
+    }, 500 / callsPerSecond);
   },
   secondsToTime: (seconds) => {
     seconds = Math.round(seconds);
